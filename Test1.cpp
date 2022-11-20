b09b772a4bbb1a6991bcec388f637e3556ecb591
Tri2k6 <118379241+Tri2k6@users.noreply.github.com>
2022-11-20 22:35:52 +0700

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -1,32 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define input cin
-#define output cout
-#define ll long long 
-#define pii pair<ll,ll>
-#define endl '\n'
-#define all(v) v.begin(),v.end()
-#define sorta(v) sort(v.begin(),v.end())
-#define mem(a,b) memset(a,b,sizeof(a));
-#define GetBit(u) u & - u;
-
-const ll cs = 2e5 + 7;
-const ll mod = 1e9 + 7;
-const ll INF = mod;
-const ll maxN = 5e3 + 7;
-const int block_size = 350;
-
-int n;
-int a[cs];
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-	cout.tie(NULL);
-    input >> n;
-    for (int i = 0;i<n;i++) {
-        output << i << endl;
-    }
-    return 0;
+"FileGoc": {
+  "prefix": "FileGoc",
+  "body": [
+    "#include <bits/stdc++.h>",
+    "using namespace std;",
+    "",
+    "#define input cin",
+    "#define output cout",
+    "#define ll long long ",
+    "#define pii pair<ll,ll>",
+    "#define endl '\\n'",
+    "#define all(v) v.begin(),v.end()",
+    "#define sorta(v) sort(v.begin(),v.end())",
+    "#define mem(a,b) memset(a,b,sizeof(a));",
+    "#define GetBit(u) u & - u;",
+    "",
+    "const ll cs = 2e5 + 7;",
+    "const ll mod = 1e9 + 7;",
+    "const ll INF = mod;",
+    "const ll maxN = 5e3 + 7;",
+    "const int block_size = 350;",
+    "",
+    "",
+    "int main() {",
+    "    ios_base::sync_with_stdio(false);",
+    "    cin.tie(NULL);",
+    "    cout.tie(NULL);",
+    "    return 0;",
+    "}"
+  ],
+  "description": "FileGoc"
 }
