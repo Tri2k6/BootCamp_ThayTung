03dd818f8364862d1da34e080c29b136d9b09122
Tri2k6 <118379241+Tri2k6@users.noreply.github.com>
2022-11-22 09:15:52 +0700

    CoSoLapTrinh_22/11/2022

--- CoSoLapTrinh.cpp
+++ CoSoLapTrinh.cpp
@@ -46,7 +46,7 @@ ll Prefix[cs];
 void Build() {
     Prefix[0] = 0;
     Prefix[1] = 1;
-    for (int i = 2;i<=18;i++) {
+    for (int i = 2;i<=19;i++) {
         Prefix[i] = Prefix[i - 1] * 10 + 1;
     }
 }
