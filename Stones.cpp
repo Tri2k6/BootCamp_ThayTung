75cc3b00c6e93471b9db1ac2eb97712359b6018a
Tri2k6 <118379241+Tri2k6@users.noreply.github.com>
2022-11-22 14:48:55 +0700

    Create Stones.cpp

---
+++ Stones.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize(3)
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC target("avx","sse2")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#include <ext/pb_ds/assoc_container.hpp>
+
+template<class integer>
+inline integer to_int(const string& s, size_t* idx = 0, int base = 10) {
+    size_t n = s.size(), i = idx ? *idx : 0; bool sign = false; integer x = 0;
+    if (s[i] == '-')
+        ++i, sign = true;
+    function<int(char)> char_to_digit = [&](char c) {
+        static const int d[] = {'a'-10,'0'}; 
+        return tolower(c)-d[isdigit(c)]; };
+    while (i < n)
+        x *= base, x += char_to_digit(s[i++]);
+    if (idx)
+        *idx = i; 
+    return sign ? -x : x; }
+ 
+template<class integer>
+inline string to_string(integer x, int base = 10) {
+    bool sign = false; string s;
+    if (x < 0)
+        x = -x, sign = true;
+    function<char(int)> digit_to_char = [](int d) {
+        static const char c[] = {'a'-10,'0'};
+        return c[d < 10]+d; };
+    do 
+        s += digit_to_char(x%base), x /= base;
+    while (x > 0); 
+    if (sign)
+        s += '-';
+    reverse(s.begin(),s.end());
+    return s; }
+ 
+template<class integer>
+inline istream& read(istream& is, integer& x) {
+    string s; is >> s, x = to_int<integer>(s); return is; }
+ 
+template<class integer>
+inline ostream& write(ostream& os, integer x) { return os << to_string(x); }
+ 
+using  lll =   signed __int128; 
+using ulll = unsigned __int128;
+ 
+inline istream& operator>>(istream& is,  lll &x) { return  read(is,x); }
+inline istream& operator>>(istream& is, ulll &x) { return  read(is,x); }
+inline ostream& operator<<(ostream& os,  lll  x) { return write(os,x); }
+inline ostream& operator<<(ostream& os, ulll  x) { return write(os,x); }
+
+#define input cin
+#define output cout
+#define ll long long 
+#define pii pair<ll,ll>
+#define endl '\n'
+#define all(v) v.begin(),v.end()
+#define sorta(v) sort(v.begin(),v.end())
+#define mem(a,b) memset(a,b,sizeof(a));
+#define GetBit(u) u & - u;
+#define bit(u,i) ((u >> i) & 1)
+#define mask(i) (1ll << i)
+#define vi vector<ll>
+#define int128 __int128
+
+const ll cs = 2e5 + 7;
+const ll mod = 1e9 + 7;
+const ll INF = mod;
+const ll maxN = 5e3 + 7;
+const int block_size = 350;
+
+int n;
+int a[cs];
+int p[cs];
+
+ll gcd(ll a, ll b) {
+    if (!b) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    input >> n;
+    for (int i = 0;i <n;i++) {
+        input >> a[i];
+    }
+    ll g = 0;
+    for (int i = 0;i<n;i++) {
+        input >> p[i];
+        g = gcd(p[i],g);
+    }
+    for (int i = 0;i<n;i++) {
+        p[i] /= g;
+    }
+    auto Tinh = [&] (ll mid) {
+        ll sum = 0;
+        for (int i = 0;i<n;i++) {
+            sum += abs(a[i] - mid * p[i]);
+        }
+        return sum;
+    };
+    ll left = 1;
+    ll right = 1e9;
+    ll res = LLONG_MAX;
+    while (left < right) {
+        ll mid = (left + right) >> 1;
+        if (Tinh(mid) > Tinh(mid + 1)) left = mid + 1;
+        else right = mid; 
+    }
+    output << Tinh(left);
+    return 0;
+}
